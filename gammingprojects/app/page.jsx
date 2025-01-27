73495c520bf592e476babd5ce3fd634b7d3bfc00
Renan Costa <renan.costa.bsi@gmail.com>
2025-01-27 15:38:08 -0300

    Agora vai porra

--- gammingprojects/app/page.jsx
+++ gammingprojects/app/page.jsx
@@ -7,7 +7,6 @@ export default async function Home() {
     <div className="bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 min-h-screen py-12 px-6">
       <h1 className="text-center text-5xl font-extrabold text-white mb-12">Gaming Platforms</h1>
 
-      {/* Grid de Plataformas */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 max-w-screen-xl mx-auto">
         {data.results.map((platform) => (
           <div
@@ -15,22 +14,20 @@ export default async function Home() {
             className="bg-gray-800 rounded-3xl overflow-hidden shadow-lg transform transition-transform hover:scale-105 hover:shadow-2xl hover:bg-gray-700"
           >
             <div className="relative">
-              {/* Imagem de fundo */}
+
               <div
                 className="w-full h-40 bg-cover bg-center rounded-t-3xl"
                 style={{ backgroundImage: `url(${platform.image_background})` }}
               ></div>
 
               <div className="p-6 space-y-4">
-                {/* Título da plataforma */}
+
                 <h2 className="text-3xl font-semibold text-white truncate">{platform.name}</h2>
 
-                {/* Informações de jogos */}
                 <p className="text-gray-400 text-sm">
                   <span className="font-semibold text-gray-300">Total Games:</span> {platform.games_count}
                 </p>
 
-                {/* Top Games */}
                 <div>
                   <h3 className="text-gray-300 text-sm font-semibold mb-2">Top Games:</h3>
                   <ul className="space-y-2">
